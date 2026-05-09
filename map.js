349b8f6b586bcadac91d4864445ee9355ce354e0
livemuseum <bazavoentorg@gmail.com>
2026-05-09 05:27:18 -0500

    Update map.js

--- map.js
+++ map.js
@@ -1,34 +1,6 @@
-console.log("MAP JS START SAFE MODE");
+console.log("JS WORKS 1");
 
-// ждём пока Leaflet точно загрузится
-function init() {
+const a = 1;
+const b = 2;
 
-  if (typeof L === "undefined") {
-    console.log("Leaflet not ready, retry...");
-    setTimeout(init, 100);
-    return;
-  }
-
-  console.log("Leaflet OK");
-
-  const map = L.map('map').setView([41.8781, -87.6298], 11);
-
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; OpenStreetMap'
-  }).addTo(map);
-
-  const test = [
-    [41.8781, -87.6298],
-    [41.8819, -87.6278],
-    [41.885, -87.62]
-  ];
-
-  test.forEach(p => {
-    L.marker(p).addTo(map)
-      .bindPopup("TEST POINT");
-  });
-
-  console.log("MAP READY");
-}
-
-init();
+console.log("SUM:", a + b);
